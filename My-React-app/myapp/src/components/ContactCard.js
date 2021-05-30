6779514a4994e6e0d7f62b4182ed171852f095c7
17BIT051-Shabirahamed <58898895+17BIT051-Shabirahamed@users.noreply.github.com>
2021-05-30 19:30:45 +0530

    My new Contact App

---
+++ My-React-app/myapp/src/components/ContactCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../images/user.jfif";
+
+const ContactCard = (props) => {
+  const { id, name, email } = props.contact;
+  return (
+    <div className="item">
+      <img className="avatar image" src={user} alt="user" width="55px"/>
+      <div className="content">
+        <Link
+          to={{ pathname: `/contact/${id}`, state: { contact: props.contact } }}
+        >
+          <div className="ui header">{name}</div>
+          <div>{email}</div>
+        </Link>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px", marginLeft: "10px" }}
+        onClick={() => props.clickHander(id)}
+      ></i>
+      <Link to={{ pathname: `/edit`, state: { contact: props.contact } }}>
+        <i
+          className="edit alternate outline icon"
+          style={{ color: "blue", marginTop: "7px" }}
+        ></i>
+      </Link>
+    </div>
+  );
+};
+
+export default ContactCard;
