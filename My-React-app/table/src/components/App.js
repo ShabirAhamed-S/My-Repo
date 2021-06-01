858e24ada34f63cd4bdab515b780c32cfcc257a8
17BIT051-Shabirahamed <58898895+17BIT051-Shabirahamed@users.noreply.github.com>
2021-06-01 18:25:11 +0530

    Update Table

--- My-React-app/table/src/components/App.js
+++ My-React-app/table/src/components/App.js
@@ -1,58 +1,100 @@
 import React from 'react';
 import './App.css';
-import Todo from './Todo'
+import Table from './Table'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      task: '',
+      name: '',
       id: 0,
+      email: '',
+      phone: '',
       item: [],
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  shouldComponentUpdate() {
+    console.log('shouldupdate')
+    return true;
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     let num = this.state.item.length + 1
-    if (this.state.task.length === 0 ) {
+    if (this.state.name.length === 0 || this.state.email.length === 0 || this.state.phone.length === 0) {
       alert('All fields are required')
       return;
     }
-    if(this.state.task.length <= 20){
-    alert('Enter valid Task Name and Task Description minimum 10 characters')
-    return
-    }
     const newItem = {
-      task: this.state.task,
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
       id: num
     };
 
     this.setState(state => ({
       item: state.item.concat(newItem),
-      task: '',
+      name: '',
+      email: '',
+      phone: ''
     }));
+  }
+
+  getSnapshotBeforeUpdate() {
+    document.getElementById("div1").innerHTML =
+      "Before the update, the favorite was ";
+    console.log('getbeforesnap')
+  }
+
+  componentDidMount() {
+    console.log('didmount')
+  }
+
+
+  componentWillUnmount() {
+    alert('unmount')
+  }
+
+
+  static getDerivedStateFromProps() {
+    console.log('getDerived')
+    return;
   }
 
   render(props) {
     return (
-      <div className='header'>
-        <center><h3>TODO</h3>
-          <Todo {...props} item={this.state.item} />
-          <br></br>
 
+      <div className='header' id='div1'>
+        <center><h3>TABLE</h3>
+          <Table {...props} item={this.state.item} />
           <form onSubmit={this.handleSubmit}>
-            <label><b> Write Todo Task </b></label>
-            <input className='input'
+            <label>
+            </label>
+            <input
+              placeholder='name'
               required
-              onChange={(e) => this.setState({ task: e.target.value })}
-              value={this.state.task}
+              onChange={(e) => this.setState({ name: e.target.value })}
+              value={this.state.name}
+            />
+            <input
+              placeholder='email'
+              required
+              onChange={(e) => this.setState({ email: e.target.value })}
+              value={this.state.email}
+            />
+            <input
+              placeholder='phone'
+              required
+              onChange={(e) => this.setState({ phone: e.target.value })}
+              value={this.state.phone}
             />
             <br></br>
             <button> Add </button>
           </form>
+
         </center>
       </div>
     );
