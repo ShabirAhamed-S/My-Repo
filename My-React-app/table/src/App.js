80407c64fe55a9fa2219dc32fb039de67ffbfbbf
17BIT051-Shabirahamed <58898895+17BIT051-Shabirahamed@users.noreply.github.com>
2021-06-01 09:30:35 +0530

    Table

---
+++ My-React-app/table/src/App.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import './App.css';
+
+const Table = (props) => {
+  return (
+    <table>
+      <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+
+      </thead>
+      <tbody>
+        {props.item.map(item => (
+          <tr>
+            <td key={item.id}>{item.id}</td>
+            <td key={item.name}>{item.name}</td>
+            <td key={item.email}>{item.email}</td>
+            <td key={item.phone}>{item.phone}</td>
+
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      id: 0,
+      email: '',
+      phone: '',
+      item: [],
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldupdate')
+    return true;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let num = this.state.item.length + 1
+    if (this.state.name.length === 0 || this.state.email.length === 0 || this.state.phone.length === 0) {
+      alert('All fields are required')
+      return;
+    }
+    const newItem = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      id: num
+    };
+
+    this.setState(state => ({
+      item: state.item.concat(newItem),
+      name: '',
+      email: '',
+      phone: ''
+    }));
+  }
+
+  getSnapshotBeforeUpdate() {
+    document.getElementById("div1").innerHTML =
+      "Before the update, the favorite was ";
+    console.log('getbeforesnap')
+  }
+
+  componentDidMount() {
+    console.log('didmount')
+  }
+
+
+  componentWillUnmount() {
+    alert('unmount')
+  }
+
+
+  static getDerivedStateFromProps() {
+    console.log('getDerived')
+    return;
+  }
+
+  render(props) {
+    return (
+
+      <div className='header' id='div1'>
+        <center><h3>TABLE</h3>
+          <Table {...props} item={this.state.item} />
+          <form onSubmit={this.handleSubmit}>
+            <label>
+            </label>
+            <input
+              placeholder='name'
+              required
+              onChange={(e) => this.setState({ name: e.target.value })}
+              value={this.state.name}
+            />
+            <input
+              placeholder='email'
+              required
+              onChange={(e) => this.setState({ email: e.target.value })}
+              value={this.state.email}
+            />
+            <input
+              placeholder='phone'
+              required
+              onChange={(e) => this.setState({ phone: e.target.value })}
+              value={this.state.phone}
+            />
+            <br></br>
+            <button> Add </button>
+          </form>
+
+        </center>
+      </div>
+    );
+  }
+}
+
+export default App;
